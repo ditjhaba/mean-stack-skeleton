deff4997a9662b4ccf308c0bb274ab6de3bffee4
Ditjhaba Selemela <ditjhaba@Ditjhabas-MacBook-Pro.local>
2014-02-27 14:30:48 +0200

    Adding AngularJS controller and two way data binding

---
+++ public/javascripts/controllers/TodoListController.js
@@ -0,0 +1,11 @@
+function TodoListController($scope){
+	$scope.todos ] = [];
+
+	$scope.doneFilter = { done : true };
+	$scope.notDoneFilter =	{ done : false };
+
+	$scope.setTodos = function(todos){
+		$scope.todos = todos;
+	}
+
+}
